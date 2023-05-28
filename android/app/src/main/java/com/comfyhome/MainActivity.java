cd8c549bab8634371b5e949bbf702b1d57d97403
Anurag Jain <mailto:anuragjain@oyelabs.com>
2023-05-28 14:04:45 +0530

    IOS setup and navigation, reanimated installation

--- android/app/src/main/java/com/comfyhome/MainActivity.java
+++ android/app/src/main/java/com/comfyhome/MainActivity.java
@@ -1,5 +1,5 @@
 package com.comfyhome;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +14,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "comfyhome";
+  }
+
+  // React native navigation setup (react-native-screens additional configuration)
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
